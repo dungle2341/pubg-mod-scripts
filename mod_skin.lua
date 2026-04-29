d1d345ab95a800f701c25cef70a48e46f9bc9671
dungle2341 <dungleios123@gmail.com>
2026-04-30 00:18:08 +0700

    Modified by www.SourceFiles.app

--- mod_skin.lua
+++ mod_skin.lua
@@ -1,144 +1,31 @@
 -- ============================================================
--- PUBG MOBILE MOD SKIN - CONFIG ONLINE
--- Sửa file này để đổi skin, không cần compile lại tweak
+-- MOD SKIN LUA - CHẠY TRỰC TIẾP TRONG GAME ENGINE
 -- ============================================================
 
-CONFIG = {
-    version = "1.0.0",
-    mod_skin_enabled = true,
-    kill_message_enabled = true,
-    dead_box_enabled = true,
-    vehicle_effect_enabled = true,
-    vehicle_effect_id = 7303001
-}
+local function applySkin(weaponID, skinID)
+    local SkinManager = require("client.module_framework.SkinManager")
+    if SkinManager then
+        SkinManager.AddSkinData(weaponID, skinID)
+        SkinManager.SetSkinOwned(weaponID, skinID, true)
+        SkinManager.SelectSkin(weaponID, skinID)
+    end
+end
 
--- SKIN VŨ KHÍ (selected = index skin đang chọn, bắt đầu từ 0)
-WEAPONS = {
-    M416 = {selected = 0, skins = {
-        {name = "Default", item_id = 101004},
-        {name = "Glacier M416", item_id = 1101004046},
-        {name = "The Fool M416", item_id = 1101004062},
-        {name = "Lizard Roar M416", item_id = 1101004078},
-        {name = "Pharaoh M416", item_id = 1101004138},
-        {name = "Crimson M416", item_id = 1101004163},
-        {name = "Shinobi M416", item_id = 1101004201},
-        {name = "Tidal Embrace M416", item_id = 1101004209}
-    }},
-    AKM = {selected = 0, skins = {
-        {name = "Default", item_id = 101001},
-        {name = "Glacier AKM", item_id = 1101001089},
-        {name = "Hellfire AKM", item_id = 1101001174},
-        {name = "Dragon AKM", item_id = 1101001213}
-    }},
-    SCAR_L = {selected = 0, skins = {
-        {name = "Default", item_id = 101003},
-        {name = "Glacier SCAR", item_id = 1101003057},
-        {name = "Blood Oath SCAR", item_id = 1101003119},
-        {name = "Soul Reaper SCAR", item_id = 1101003167}
-    }},
-    M762 = {selected = 0, skins = {
-        {name = "Default", item_id = 101008},
-        {name = "Glacier M762", item_id = 1101008026},
-        {name = "8-bit Unicorn M762", item_id = 1101008051},
-        {name = "Dragon Breath M762", item_id = 1101008116}
-    }},
-    AWM = {selected = 0, skins = {
-        {name = "Default", item_id = 103003},
-        {name = "Glacier AWM", item_id = 1103003022},
-        {name = "Godzilla AWM", item_id = 1103003051}
-    }},
-    GROZA = {selected = 0, skins = {
-        {name = "Default", item_id = 101005},
-        {name = "Glacier Groza", item_id = 1101005019},
-        {name = "Poseidon Groza", item_id = 1101005052}
-    }},
-    AUG = {selected = 0, skins = {
-        {name = "Default", item_id = 101006},
-        {name = "Glacier AUG", item_id = 1101006033}
-    }},
-    M16A4 = {selected = 0, skins = {
-        {name = "Default", item_id = 101002},
-        {name = "Glacier M16", item_id = 1101002029}
-    }},
-    ACE32 = {selected = 0, skins = {
-        {name = "Default", item_id = 101102},
-        {name = "Glacier ACE32", item_id = 1101102007}
-    }},
-    KAR98 = {selected = 0, skins = {
-        {name = "Default", item_id = 103001},
-        {name = "Glacier Kar98", item_id = 1103001060}
-    }},
-    M24 = {selected = 0, skins = {
-        {name = "Default", item_id = 103002},
-        {name = "Glacier M24", item_id = 1103002018}
-    }},
-    DP28 = {selected = 0, skins = {
-        {name = "Default", item_id = 105002},
-        {name = "Glacier DP28", item_id = 1105002018}
-    }},
-    M249 = {selected = 0, skins = {
-        {name = "Default", item_id = 105001},
-        {name = "Glacier M249", item_id = 1105001020}
-    }},
-    UZI = {selected = 0, skins = {
-        {name = "Default", item_id = 102001},
-        {name = "Pharaoh UZI", item_id = 1102001058}
-    }},
-    UMP = {selected = 0, skins = {
-        {name = "Default", item_id = 102002},
-        {name = "Glacier UMP", item_id = 1102002043}
-    }},
-    VECTOR = {selected = 0, skins = {
-        {name = "Default", item_id = 102003},
-        {name = "Glacier Vector", item_id = 1102003020}
-    }},
-    PAN = {selected = 0, skins = {
-        {name = "Default", item_id = 108004},
-        {name = "Honeypot Pan", item_id = 1108004125},
-        {name = "Chicken Hot Pan", item_id = 1108004356}
-    }}
-}
+-- Áp dụng skin khi vào game
+local function OnEnterGame()
+    applySkin(101004, 1101004046)  -- M416 Glacier
+    applySkin(101001, 1101001089)  -- AKM Glacier
+    applySkin(101003, 1101003057)  -- SCAR-L Glacier
+    applySkin(101008, 1101008026)  -- M762 Glacier
+    applySkin(103003, 1103003022)  -- AWM Glacier
+    print("[ModSkin] Skins applied!")
+end
 
--- SKIN XE
-VEHICLES = {
-    DACIA = {selected = 0, skins = {
-        {name = "Default", item_id = 1903001},
-        {name = "Pharaoh Dacia", item_id = 1903076},
-        {name = "Crimson Dacia", item_id = 1903071}
-    }},
-    UAZ = {selected = 0, skins = {
-        {name = "Default", item_id = 1908001},
-        {name = "Pharaoh UAZ", item_id = 1908067}
-    }},
-    COUPE = {selected = 0, skins = {
-        {name = "Default", item_id = 1961001},
-        {name = "Pharaoh Coupe", item_id = 1961047}
-    }},
-    MOTORCYCLE = {selected = 0, skins = {
-        {name = "Default", item_id = 1901001},
-        {name = "Pharaoh Motor", item_id = 1901073}
-    }}
-}
+-- Hook vào sự kiện vào game
+local O_ReceiveBeginPlay = GameFrontendHUD.ReceiveBeginPlay
+GameFrontendHUD.ReceiveBeginPlay = function(self)
+    O_ReceiveBeginPlay(self)
+    OnEnterGame()
+end
 
--- SKIN TRANG PHỤC
-OUTFITS = {selected = 0, skins = {
-    {name = "Default", item_id = 403003},
-    {name = "Blood Raven X-Suit", item_id = 1405628},
-    {name = "Golden Pharaoh X-Suit", item_id = 1405870},
-    {name = "Poseidon X-Suit", item_id = 1406311},
-    {name = "Arcane Jester X-Suit", item_id = 1406475}
-}}
-
-BACKPACKS = {selected = 0, skins = {
-    {name = "Default", item_id = 501006},
-    {name = "Blood Raven", item_id = 1501003174},
-    {name = "Golden Pharaoh", item_id = 1501003220}
-}}
-
-HELMETS = {selected = 0, skins = {
-    {name = "Default", item_id = 502001},
-    {name = "Blood Raven", item_id = 1502003014},
-    {name = "Golden Pharaoh", item_id = 1502003028}
-}}
-
-print("[ModSkin] Online config loaded!")
+print("[ModSkin] Script loaded!")
