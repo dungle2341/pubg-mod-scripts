04f0b2a0c545e9ff1035aa2d7deca9896701c7d5
dungle2341 <dungleios123@gmail.com>
2026-04-30 00:18:24 +0700

    Modified by www.SourceFiles.app

--- mod_vehicle.lua
+++ mod_vehicle.lua
@@ -1,33 +1,64 @@
--- ============================================================
--- VEHICLE MOD CONFIG
--- ============================================================
+local VehicleAvatarComponent = require("GameLua.GameCore.Module.Vehicle.Component.VehicleAvatarComponent")
 
-VEHICLE_CONFIG = {
-    version = "1.0.0",
-    enable_effect = true,
-    default_effect_id = 7303001
-}
-
--- Hook CheckCanPlaySkinSwitchEffect
-function CheckCanPlaySkinSwitchEffect(curVehicleId, lastVehicleId)
+VehicleAvatarComponent.__inner_impl.CheckCanPlaySkinSwitchEffect = function(self, curVehicleId, lastVehicleId)
     return true
 end
 
--- Hook ShowVehicleSwitchEffect
-function ShowVehicleSwitchEffect(vehicleActor)
-    if not VEHICLE_CONFIG.enable_effect then
+VehicleAvatarComponent.__inner_impl.ShowVehicleSwitchEffect = function(self)
+    if not self.curSwitchEffectId or self.curSwitchEffectId <= 0 then
+        self.curSwitchEffectId = 7303001
+    end
+
+    local vehicleActor = self:GetOwner()
+    if not slua.isValid(vehicleActor) then return false end
+
+    if self.uSwitchEffectActor then
+        self:StopSkinSwitchEffect()
+        self.uSwitchEffectActor:K2_DestroyActor()
+        self.uSwitchEffectActor = nil
+    end
+
+    if not self.lastEquipedAvatarId or self.lastEquipedAvatarId <= 0 then
+        self.lastEquipedAvatarId = vehicleActor.ClientUsedAvatarID or vehicleActor:GetDefaultAvatarID() or 0
+    end
+
+    local currentAvatarID = vehicleActor.ClientUsedAvatarID or self.lastEquipedAvatarId or 0
+    local bIsLobbyActor = self:IsLobbyActor()
+    local world = slua_GameFrontendHUD:GetWorld()
+    local VehiclePlateLicenseUtil = require("GameLua.Activity.Commercialize.GamePlay.Vehicle.VehiclePlateLicenseUtil")
+    local SkinSwitchEffectActorPath = VehiclePlateLicenseUtil.GetSwitchEffectActorPath()
+    local BP_DissolveVehicleClass = import(SkinSwitchEffectActorPath)
+
+    self.uSwitchEffectActor = world:SpawnActor(BP_DissolveVehicleClass, nil, nil, nil)
+    if not slua.isValid(self.uSwitchEffectActor) then
+        self.uSwitchEffectActor = nil
         return false
     end
-    
-    print("[VehicleMod] Showing switch effect for vehicle: " .. (vehicleActor or "unknown"))
+
+    self.uSwitchEffectActor:K2_AttachToActor(vehicleActor, "None", 1, 1, 1, false)
+    self.uSwitchEffectActor:K2_SetActorRelativeLocation(FVector(0, 0, 0), false, nil, false)
+    self.uSwitchEffectActor:K2_SetActorRelativeRotation(FRotator(0, 0, 0), false, nil, false)
+    self:ChangeFakeSwitchVehicleAvatar(self.uSwitchEffectActor.Mesh, self.lastEquipedAvatarId)
+    self.uSwitchEffectActor:SetAnimInsAndAnimState(self.uOldVehicleMeshAnimClass, vehicleActor)
+    self.uSwitchEffectActor:StartVehicleSwitchEffect(vehicleActor, self.curSwitchEffectId, self.lastEquipedAvatarId, currentAvatarID, bIsLobbyActor)
+    self.uOldVehicleMeshAnimClass = nil
     return true
 end
 
--- Hook ReceiveBeginPlay
-function OnVehicleSpawn(vehicleComponent)
-    print("[VehicleMod] Vehicle spawned - resetting effects")
-    vehicleComponent.curSwitchEffectId = VEHICLE_CONFIG.default_effect_id
-    vehicleComponent.lastEquipedAvatarId = 0
+VehicleAvatarComponent.__inner_impl.ResetAnimationState = function(self)
+    if self.uSwitchEffectActor then
+        self:StopSkinSwitchEffect()
+        self.uSwitchEffectActor:K2_DestroyActor()
+        self.uSwitchEffectActor = nil
+    end
+    self.lastEquipedAvatarId = 0
+    self.curSwitchEffectId = 7303001
 end
 
-print("[VehicleMod] Vehicle script loaded!")
+local O_ReceiveBeginPlay = VehicleAvatarComponent.__inner_impl.ReceiveBeginPlay
+VehicleAvatarComponent.__inner_impl.ReceiveBeginPlay = function(self)
+    O_ReceiveBeginPlay(self)
+    self:ResetAnimationState()
+end
+
+print("[VehicleMod] Vehicle effect script loaded!")
